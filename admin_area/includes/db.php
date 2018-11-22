780973bce7e9f8668c4308c827988ac7dd142c36
triptiverma31 <32236454+triptiverma31@users.noreply.github.com>
2018-11-22 11:18:07 +0530

    Update db.php

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "khanstore";
+$db = "smartstore";
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
 
@@ -10,6 +10,4 @@ $con = mysqli_connect($servername, $username, $password,$db);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 ?>
