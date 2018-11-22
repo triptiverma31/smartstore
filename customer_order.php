6243963c65049a0a0cc4197e253b045cd0655597
Vikrant SIngh <vikrant.mnnit00@gmail.com>
2018-11-22 11:29:54 +0530

    Update customer_order.php

--- customer_order.php
+++ customer_order.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION["uid"])){
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Khan Store</title>
+		<title>Smart Store</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
 		<script src="js/bootstrap.min.js"></script>
@@ -20,9 +20,9 @@ if(!isset($_SESSION["uid"])){
 	</head>
 <body>
 	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container-fluid">	
+		<div class="container-fluid">
 			<div class="navbar-header">
-				<a href="#" class="navbar-brand">Khan Store</a>
+				<a href="#" class="navbar-brand">Smart Store</a>
 			</div>
 			<ul class="nav navbar-nav">
 				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
@@ -34,7 +34,7 @@ if(!isset($_SESSION["uid"])){
 	<p><br/></p>
 	<p><br/></p>
 	<div class="container-fluid">
-	
+
 		<div class="row">
 			<div class="col-md-2"></div>
 			<div class="col-md-8">
@@ -68,7 +68,7 @@ if(!isset($_SESSION["uid"])){
 								}
 							}
 						?>
-						
+
 					</div>
 					<div class="panel-footer"></div>
 				</div>
@@ -78,51 +78,3 @@ if(!isset($_SESSION["uid"])){
 	</div>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
