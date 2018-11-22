9b0872878b281e3c345600a10c6affca93847c40
triptiverma31 <32236454+triptiverma31@users.noreply.github.com>
2018-11-22 11:18:14 +0530

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -19,7 +19,7 @@ if (isset($_POST["login_user_with_product"])) {
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>Khan Store</title>
+		<title>Smart Store</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css"/>
 		<script src="js/jquery2.js"></script>
 		<script src="js/bootstrap.min.js"></script>
@@ -33,7 +33,7 @@ if (isset($_POST["login_user_with_product"])) {
 	<div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">	
 			<div class="navbar-header">
-				<a href="#" class="navbar-brand">Khan Store</a>
+				<a href="#" class="navbar-brand">Smart Store</a>
 			</div>
 			<ul class="nav navbar-nav">
 				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
