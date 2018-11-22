4cea18de8000b7543a0a0295062af6d044dfbcf8
Vikrant SIngh <vikrant.mnnit00@gmail.com>
2018-11-22 12:08:07 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -131,7 +131,7 @@ if(!isset($_SESSION["uid"])){
 							</div>
 						</div> -->
 					</div>
-					<div class="panel-footer">&copy; 2016</div>
+					<div class="panel-footer">&copy; 2018</div>
 				</div>
 			</div>
 			<div class="col-md-1"></div>
